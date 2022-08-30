75ef09f401906eec03558bc594224074f0a91f13
xudonglin <xu.dong.lin@finsiot.com>
2022-08-30 18:34:07 +0800

    测试Iterator的用法

--- src/test/java/com/xdl/AppTest.java
+++ src/test/java/com/xdl/AppTest.java
@@ -1,20 +1,34 @@
 package com.xdl;
 
-import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
+public class AppTest {
     /**
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void testIterator() {
+        HashSet<String> nameSet = new HashSet<>();
+        nameSet.add("xdl");
+        nameSet.add("gj");
+        Iterator<String> iterator = nameSet.iterator();
+        while (iterator.hasNext()) {
+            String name = iterator.next();
+            System.out.println(name);
+            if (name == "xdl") {
+                iterator.remove(); // 将会删除nameSet中的xdl元素
+            }
+        }
+        for (String name : nameSet) {
+            System.out.println(name);
+        }
     }
 }
