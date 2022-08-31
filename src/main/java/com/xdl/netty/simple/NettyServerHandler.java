3368e73612aa7d8384ee7b51974c3def2dba638e
xudonglin <xu.dong.lin@finsiot.com>
2022-08-31 16:46:34 +0800

    自定义任务

--- src/main/java/com/xdl/netty/simple/NettyServerHandler.java
+++ src/main/java/com/xdl/netty/simple/NettyServerHandler.java
@@ -19,17 +19,35 @@ public class NettyServerHandler extends ChannelInboundHandlerAdapter {
      */
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        //如果这里是一个非常耗时的业务，将会被阻塞在这里。解决办法是：异步执行将处理业务的代码提交到该channel对应的NioEventLoop的taskQueue中
+        //ctx.writeAndFlush(Unpooled.copiedBuffer("服务器收到请求，正在处理数据....", CharsetUtil.UTF_8));
+        //Thread.sleep(10 * 1000);
+        // 解决方案一：用户自定义普通任务
+        System.out.println(Thread.currentThread().getName());
+        // 这里的任务队列并不是一个新的线程,队列中的多个任务会排队执行
+        ctx.channel().eventLoop().execute(() -> {
+            System.out.println(Thread.currentThread().getName());
+            ctx.writeAndFlush(Unpooled.copiedBuffer("服务器收到请求，正在处理数据....", CharsetUtil.UTF_8));
+            try {
+                Thread.sleep(10 * 1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            ctx.writeAndFlush(Unpooled.copiedBuffer("服务器收到请求, 完成处理....", CharsetUtil.UTF_8));
+        });
+        /**
         System.out.println("服务器读取线程 " + Thread.currentThread().getName());
         System.out.println("server ctx =" + ctx);
         System.out.println("看看channel 和 pipeline的关系");
         Channel channel = ctx.channel();
         ChannelPipeline pipeline = ctx.pipeline(); // 本质是一个双向链表，出栈入栈
         // 将msg转成一个ByteBuf，该ByteBuf是Netty提供的，不是NIO的ByteBuffer
-        ByteBuf buf = (ByteBuf) msg;
+         ByteBuf buf = (ByteBuf) msg;
         System.out.println("客户端发送消息是：" + buf.toString(CharsetUtil.UTF_8));
         System.out.println("客户端地址：" + channel.remoteAddress());
+         */
     }
-    //数据读取完成
+    //数据读取完成，不会关心channelRead中异步任务是否执行完成
     @Override
     public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
         //将数据写入到缓存，并刷新
--- src/main/java/com/xdl/netty/simple/Test.java
+++ src/main/java/com/xdl/netty/simple/Test.java
@@ -5,5 +5,22 @@ import io.netty.util.NettyRuntime;
 public class Test {
     public static void main(String[] args) {
         System.out.println("计算机CPU核数：" + NettyRuntime.availableProcessors());
+        A a = new A();
+        System.out.println(a.name);
+
+    }
+}
+
+class A {
+    String name;
+
+    public A(String name) {
+        System.out.println("有参构造被执行");
+        this.name = name;
+    }
+
+    public A() {
+        // this("xdl"); // 会去调用有参的构造方法
+        this.name = "xdl";
     }
 }
